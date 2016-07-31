5972747429ef32907faf51b3d065dcabacac5b02
aliken9 <aliken9@gmail.com>
2016-07-30 20:23:12 -0400

    Test scene basic WIP

---
+++ TweenToggle/Assets/Scripts/TweenToggleExamples.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenToggleExamples : MonoBehaviour {
+
+	public TweenToggle positionTweenToggle;
+	public TweenToggle rotationTweenToggle;
+	public TweenToggle alphaTweenToggle;
+	public TweenToggle scaleTweenToggle;
+
+	public void OnPositionToggleButton(bool isOn){
+		if(isOn){
+			positionTweenToggle.Show();
+		}
+		else{
+			positionTweenToggle.Hide();
+		}
+	}
+
+	public void OnRotationToggleButton(bool isOn){
+		if(isOn){
+			rotationTweenToggle.Show();
+		}
+		else{
+			rotationTweenToggle.Hide();
+		}
+	}
+
+}
