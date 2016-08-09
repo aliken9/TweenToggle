f06cbeb8a1eb3683246300904e9295ee4ac793d6
aliken9 <aliken9@gmail.com>
2016-08-08 18:48:53 -0700

    New demux scene

---
+++ TweenToggle/Assets/Scripts/TweenToggleDemuxExamples.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenToggleDemuxExamples : MonoBehaviour {
+	public TweenToggleDemux demux1;
+	public TweenToggleDemux demux2;
+	public TweenToggleDemux demux3;
+	public TweenToggleDemux demux4;
+
+	public void OnDemux1Button(bool isOn){
+		if(isOn){
+			demux1.Show();
+		}
+		else{
+			demux1.Hide();
+		}
+	}
+
+	public void OnDemux2Button(bool isOn){
+		if(isOn){
+			demux2.Show();
+		}
+		else{
+			demux2.Hide();
+		}
+	}
+
+	public void OnDemux3Button(bool isOn){
+		if(isOn){
+			demux3.Show();
+		}
+		else{
+			demux3.Hide();
+		}
+	}
+
+	public void OnDemux4Button(bool isOn){
+		if(isOn){
+			demux4.Show();
+		}
+		else{
+			demux4.Hide();
+		}
+	}
+}
